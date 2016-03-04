8faac9bb3ae2b79b3a74ef9a4e5b675f369f19f5
yangxi <1006652872@qq.com>
2016-03-04 10:36:38 +0800

    test

--- src/main/java/com/tuling/XiaoBai.java
+++ src/main/java/com/tuling/XiaoBai.java
@@ -7,7 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Map;
-import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
@@ -16,28 +15,24 @@ import com.alibaba.fastjson.JSON;
 public class XiaoBai {
 	private static final Logger logger = Logger.getLogger(XiaoBai.class);
 	public static void main(String[] args) throws IOException {
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			String question = scanner.next();
-			System.out.println(getAnswer(question));
-		}
+		System.out.println(getAnswer("我们什么时候认识的"));
 		
 	}
 	
-	public static String getAnswer(String question){
+	public static String getAnswer(String question) {
 		String APIKEY = "9753cfbae7e980c035a640b51c0b5687";
 		String INFO = "";
+		String userId = "ouyangxidexiaobai";
 		StringBuffer sb = new StringBuffer();
 		try {
 			INFO = URLEncoder.encode(question, "utf-8");
-			String getURL = "http://www.tuling123.com/openapi/api?key=" + APIKEY
-					+ "&info=" + INFO;
+			String getURL = "http://www.tuling123.com/openapi/api?key="
+					+ APIKEY + "&info=" + INFO+"&userid="+userId;
 			URL getUrl = new URL(getURL);
 			HttpURLConnection connection = (HttpURLConnection) getUrl
 					.openConnection();
 			connection.connect();
-			
+
 			// 取得输入流，并使用Reader读取
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					connection.getInputStream(), "utf-8"));
@@ -49,12 +44,11 @@ public class XiaoBai {
 			// 断开连接
 			connection.disconnect();
 		} catch (Exception e) {
-			logger.error("小白出事了！");
 			e.printStackTrace();
 		}
 		String result = sb.toString();
-		@SuppressWarnings("unchecked")
 		Map<String, String> resultMap = JSON.parseObject(result, Map.class);
 		return resultMap.get("text");
 	}
+	
 }
